8e528acff68b78be6a710cebd0f905d3bc5936e8
pinguatnull <116451803+pinguatnull@users.noreply.github.com>
2022-10-23 10:33:28 +0530

    Add files via upload

---
+++ MemePage.py
@@ -0,0 +1,30 @@
+from urllib import response
+from flask import Flask, render_template, request, send_file
+import requests
+import json
+import socket
+import os
+import time
+
+app = Flask(__name__)
+
+def get_meme():
+    url ="https://meme-api.herokuapp.com/gimme/school_memes"
+    response = json.loads(requests.request("GET",url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+    return response
+
+@app.route("/")
+def index():
+    meme_pic,subreddit = get_meme()
+    return  render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit)
+
+@app.route('/download')
+def download():
+   return send_file('hi.txt', as_attachment=True)
+
+app.run(host = "0.0.0.0", port=80)
+
+      
